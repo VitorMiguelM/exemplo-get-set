8133ef0074a450e90c625bb09ff6b94c5edf86cf
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-06-04 16:59:24 -0300

    .

---
+++ ExempoloGetSet/Cachorro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempoloGetSet
+{
+    class Cachorro
+    {
+        private string Nome;
+        public string Raca;
+        public double Peso;
+        private double Altura;
+
+        public void SetNome(string nome)
+        {
+            if (nome.Count() < 3)
+            {
+                throw new Exception("Nome deve conter 3 letras");
+            }
+            if (nome.Count() > 40)
+            {
+                throw new Exception("Nome deve conter no máximo 40 caracteres");
+            }
+            Nome = nome;
+        }
+
+        public void SetAltura(double altura)
+        {
+            if (altura <= 0)
+            {
+                throw new Exception("Altura deve ser maior que 0");
+            }
+            if (altura > 2)
+            {
+                throw new Exception("Altura deve ser menor que 2 metros");
+            }
+            Altura = altura;
+        }
+    }
+}
---
+++ ExempoloGetSet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempoloGetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro dogao = new Cachorro();
+            // set tem como objetivo guardar determinada informação
+            dogao.SetNome("aaaaaaa");
+            dogao.SetAltura(1.10);
+            dogao.Peso = 80;
+            dogao.Raca = "Pinscher";
+        }
+    }
+}
